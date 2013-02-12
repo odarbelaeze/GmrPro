52b19e84e627ee3f8ec56971e278757ebea609eb
Oscar David Arbeláez <odarbelaeze@gmail.com>
2013-02-12 00:23:50 -0500

    Rady for the last try :)

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 #include "GmrPro.h"
-#include <cassert>
+#include <sstream>
 
 int main(int argc, char const *argv[])
 {
@@ -10,13 +10,9 @@ int main(int argc, char const *argv[])
     try
     {    
         if (argc == 1)
-        {
             system = System("docs/descriptor_example.json");
-        }
         else
-        {
             system = System(argv[1]);
-        }
     }
     catch (BadDescriptorException& e)
     {
@@ -26,9 +22,72 @@ int main(int argc, char const *argv[])
 
     system.addField("Electric", electricField);
 
-    for (int i = 0; i < 20; ++i)
+    std::cout << "System ready, startng runs." << std::endl;
+
+    Json::Reader reader;
+    Json::Value  root;
+    Json::Value  runs;
+    std::ifstream runsFile;
+    std::ofstream runsDataFile;
+    char dataFileName[20];
+
+    if (argc == 3)
+        runsFile.open(argv[2]);
+    else if (argc == 2)
+        runsFile.open(argv[1]);
+    else
+        runsFile.open("docs/descriptor_example.json");
+
+    bool parsingSuccessful = reader.parse(runsFile, root, false);
+    if (!parsingSuccessful)
     {
-        system.monteCarloThermalStep(false);
+        std::cout  << "Failed to parse configuration\n"
+                   << reader.getFormattedErrorMessages();
+        throw BadDescriptorException("The descriptor is not a valid json file.");
+    }
+    runsFile.close();
+
+    runs = root["runs"];
+    int nRuns = runs.size();
+
+    std::cout << "Ready to run " << nRuns << " simulations over the system."
+              << std::endl;
+
+    for (int i = 0; i < nRuns; ++i)
+    {
+        std::cout << "Runing " << i + 1 << " out of " << nRuns << "..."
+                  << std::endl;
+        
+        std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+        std::ostringstream strCout;
+        std::cout.rdbuf( strCout.rdbuf() );
+
+        electricField = Vector(
+            runs[i]["electricField"][0].asFloat(),
+            runs[i]["electricField"][1].asFloat(),
+            runs[i]["electricField"][2].asFloat()
+        );
+
+        system.setThermalEnergy(runs[i]["thermalEnergyDelta"].asFloat() * 
+            runs[i]["multiplier"].asInt());
+
+        for (int iii = 0; iii < runs[i]["multiplier"].asInt(); ++iii)
+        {        
+            for (int thermalStep = 0; thermalStep < runs[i]["thermalSteps"].asInt(); ++thermalStep)
+                system.monteCarloThermalStep(false);
+            for (int dynamicStep = 0; dynamicStep < runs[i]["dynamicSteps"].asInt(); ++dynamicStep)
+                system.monteCarloDynamicStep(dynamicStep % 10 == 0);
+            system.setThermalEnergy(system.getThermalEnergy() - 
+                                    runs[i]["thermalEnergyDelta"].asFloat() );
+        }
+
+        std::cout.rdbuf( oldCoutStreamBuf );
+        std::cout << "... OK" << std::endl;
+        
+        sprintf(dataFileName, "data/run%d", i + 1);
+        runsDataFile.open(dataFileName);
+        runsDataFile << strCout.str();
+        runsDataFile.close();
     }
 
     return 0;
